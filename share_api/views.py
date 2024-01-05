7efa7a263d1c7978ca1ed249a81b29e500a889e7
podestal <luisrodriguezpodesta@gmail.com>
2024-01-05 16:36:43 -0500

    isAdminOrReadOnly permissions

--- share_api/views.py
+++ share_api/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from . import permissions
 from . import models
 from . import serializers
 
 class ServiceViewSet(ModelViewSet):
     queryset = models.Service.objects.all()
     serializer_class = serializers.SerivceSerializer
-    # authentication_classes = 
+    permission_classes = [permissions.IsAdminOrReadOnly]
 
 class ScreeViewSet(ModelViewSet):
     queryset = models.Screen.objects.all()
@@ -15,7 +15,6 @@ class ScreeViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'account_id': self.kwargs['accounts_pk']}
-# return {'product_id': self.kwargs['product_pk']}
 
 class AccountViewSet(ModelViewSet):
     queryset = models.Account.objects.all()
