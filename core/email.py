dd15c3eef0cb1dcd36d59656d55706791d74618d
podestal <luisrodriguezpodesta@gmail.com>
2024-02-16 20:47:48 -0500

    customazing emails

---
+++ core/email.py
@@ -0,0 +1,18 @@
+from djoser.email import ActivationEmail
+from django.contrib.auth.tokens import default_token_generator
+
+from djoser import utils
+from djoser.conf import settings
+
+class ActivationEmailCustomized(ActivationEmail):
+
+    template_name = "email/activation.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.ACTIVATION_URL.format(**context)
+        return context
