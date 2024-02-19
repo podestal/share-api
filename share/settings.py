f43a227476725d242f275146e96eed31eeec183d
podestal <luisrodriguezpodesta@gmail.com>
2024-02-19 12:49:26 -0500

    secret key

--- share/settings.py
+++ share/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dhkvpy6=jvd28)(_wyp3aca+@h$h%6wh=g3(yenu&*+qmw718z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
