612542211ee0184bba6584fe0e6b907ba6d81c99
podestal <luisrodriguezpodesta@gmail.com>
2024-02-19 10:00:23 -0500

    adding allow host api

--- share/settings.py
+++ share/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.206.246.99:8000']
 
 
 # Application definition
