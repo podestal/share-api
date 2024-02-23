455926076c2ac6c5f168dc50900aa0bc2a2f79c0
podestal <luisrodriguezpodesta@gmail.com>
2024-02-23 13:17:10 -0500

    changes to deploy vercel

--- share/settings.py
+++ share/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,6 +103,11 @@ DATABASES = {
         'PASSWORD': '13anguloX',
     }
 }
+
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+# del DATABASES['default']['OPTIONS']['sslmode'] 
+# DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 
 # Password validation
